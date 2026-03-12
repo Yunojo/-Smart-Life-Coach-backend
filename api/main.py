38e3822a780878defefafbb4a2820dc65ed715e7
Yunojo <joseagutierrezlomeli@gmail.com>
2026-03-11 20:30:26 -0600

    Incluyendo prueba comunicacion Json

---
+++ api/main.py
@@ -0,0 +1,22 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.post("/Chat")
+async def recibir_mensaje(request:Request):
+    cuerpo = await request.json()
+    mensajes= cuerpo.get("mensajes",[])
+    if mensajes:
+        ultimo_mensaje=mensajes[-1]["content"]
+        print(f"Mensaje recibido: {ultimo_mensaje}")
+
+        return "Mensaje recibido en FastApi"
